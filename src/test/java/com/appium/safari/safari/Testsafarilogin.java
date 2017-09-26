d741ddb0345ea27d443eca05b2291a48afc396d6
Christiaan de Haan <christiaandehaan@Christiaans-MacBook-Pro.local>
2017-09-26 19:10:32 +0200

    Test Versioning

--- src/test/java/com/appium/safari/safari/Testsafarilogin.java
+++ src/test/java/com/appium/safari/safari/Testsafarilogin.java
@@ -35,6 +35,7 @@ dc.setCapability("reportFormat", reportFormat);
 dc.setCapability("testName", testName);
 ///
 PlatformVersion = System.getProperty("PlatformVersion");
+//PlatformVersion = "5.0.1";
 dc.setCapability(MobileCapabilityType.PLATFORM_NAME, System.getProperty("PlatformName"));
 dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, PlatformVersion);
 dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
@@ -57,7 +58,7 @@ public void LoginTest() {
 driver.findElement(By.xpath("//*[@text='Skip']")).click();
 
 //If the platform version is bellow 6 kick of the code (permissions not asked on <6)
-if (PlatformVersion.compareTo("6") < 0)
+if (versionCompare(PlatformVersion, "6") < 0)
    { 
 	driver.findElement(By.xpath("//*[@text='Skip' and ./parent::*[./following-sibling::*[./*[@text='Share your know how or expertise\n" + 
 			" by answering quiz questions and \n" + 
@@ -95,7 +96,37 @@ driver.findElement(By.xpath("//*[@id='login_button']")).click();
 new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='background' and ./*[./*[@text='Brag']]]")));
 
 }
-
+/**
+ * Compares two version strings. 
+ * 
+ * Use this instead of String.compareTo() for a non-lexicographical 
+ * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
+ * 
+ * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
+ * 
+ * @param str1 a string of ordinal numbers separated by decimal points. 
+ * @param str2 a string of ordinal numbers separated by decimal points.
+ * @return The result is a negative integer if str1 is _numerically_ less than str2. 
+ *         The result is a positive integer if str1 is _numerically_ greater than str2. 
+ *         The result is zero if the strings are _numerically_ equal.
+ */
+public int versionCompare(String str1, String str2) {
+    String[] vals1 = str1.split("\\.");
+    String[] vals2 = str2.split("\\.");
+    int i = 0;
+    // set index to first non-equal ordinal or length of shortest version string
+    while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
+      i++;
+    }
+    // compare first non-equal ordinal number
+    if (i < vals1.length && i < vals2.length) {
+        int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+        return Integer.signum(diff);
+    }
+    // the strings are equal or one string is a substring of the other
+    // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+    return Integer.signum(vals1.length - vals2.length);
+}
 
 @After
 
